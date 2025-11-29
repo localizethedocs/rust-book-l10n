251ec83655b809c487f8931fcbdc727645ba83f3
Haowei Hsu <hwhsu1231@localizethedocs.org>
2025-11-29 11:43:53 +0800

    ✨ feat(init): initialize branch 'main'
    Initialize the 'main' branch with the following files:
    
    - Add document files: README.md, LICENSE-BSD, LICENSE-CC.
    - Add GitHub workflow files: ci-check-pr-status,..., etc.
    - Add GitHub issue templates: language_request.yml,..., etc.
    - Add Git config files: .gitmodules, .gitignore.
    - Add Git submodules: cmake/modules, cmake/plugins.
    - Add CMake script files: CMakeLists.txt, cmake/**/* files.
    - Add config files: versions.json, languages.json, crowdin.yml.

---
+++ cmake/custom/ltd-config.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",      "Development"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Crowdin",   "https://ltdorgtest.crowdin.com/rust-book-l10n"],
+    ["GitHub",    "https://github.com/ltdorgtest/rust-book-l10n"],
+    ["AtomGit",   "https://atomgit.com/ltdorgtest/rust-book-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/ltdorgtest/rust-book-l10n"],
+  ]
+};
